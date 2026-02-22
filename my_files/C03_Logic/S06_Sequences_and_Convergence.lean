c65ba2a7d8505e7011d2cafca67f3b7a78a102fa
pilottinick <36806742+pilottinick@users.noreply.github.com>
2026-02-22 04:10:05 -0700

    chapter 3 section 6

--- my_files/C03_Logic/S06_Sequences_and_Convergence.lean
+++ my_files/C03_Logic/S06_Sequences_and_Convergence.lean
@@ -35,7 +35,13 @@ theorem convergesTo_add {s t : ℕ → ℝ} {a b : ℝ}
   rcases cs (ε / 2) ε2pos with ⟨Ns, hs⟩
   rcases ct (ε / 2) ε2pos with ⟨Nt, ht⟩
   use max Ns Nt
-  sorry
+  intro n nge
+  calc
+    |s n + t n - (a + b)| = |(s n - a) + (t n - b)| := by congr ; ring
+    _ ≤ |s n - a| + |t n - b| := abs_add _ _
+    _ < ε/2 + ε/2 := add_lt_add (hs n (le_of_max_le_left nge)) (ht n (le_of_max_le_right nge))
+    _ = ε := by norm_num
+
 
 theorem convergesTo_mul_const {s : ℕ → ℝ} {a : ℝ} (c : ℝ) (cs : ConvergesTo s a) :
     ConvergesTo (fun n ↦ c * s n) (c * a) := by
@@ -46,13 +52,27 @@ theorem convergesTo_mul_const {s : ℕ → ℝ} {a : ℝ} (c : ℝ) (cs : Conver
     rw [h]
     ring
   have acpos : 0 < |c| := abs_pos.mpr h
-  sorry
+  intro ε εpos
+  dsimp
+  have εcpos : 0 < ε / |c| := by apply div_pos εpos acpos
+  rcases cs (ε / |c|) εcpos with ⟨N, hs⟩
+  use N
+  intro n nge
+  calc
+    |c * s n - c * a| = |c * (s n - a)| := by congr ; ring
+    _ = |c| * |s n - a| := by apply abs_mul
+    _ < |c| * (ε / |c|) := mul_lt_mul' (le_refl _) (hs _ nge) (abs_nonneg _) acpos
+    _ = ε := by field_simp
 
 theorem exists_abs_le_of_convergesTo {s : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) :
     ∃ N b, ∀ n, N ≤ n → |s n| < b := by
   rcases cs 1 zero_lt_one with ⟨N, h⟩
   use N, |a| + 1
-  sorry
+  intro n nge
+  apply lt_add_of_sub_left_lt
+  calc
+    |s n| - |a| ≤ |s n - a| := by apply abs_sub_abs_le_abs_sub
+    _ < 1 := h n nge
 
 theorem aux {s t : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) (ct : ConvergesTo t 0) :
     ConvergesTo (fun n ↦ s n * t n) 0 := by
@@ -62,7 +82,14 @@ theorem aux {s t : ℕ → ℝ} {a : ℝ} (cs : ConvergesTo s a) (ct : Converges
   have Bpos : 0 < B := lt_of_le_of_lt (abs_nonneg _) (h₀ N₀ (le_refl _))
   have pos₀ : ε / B > 0 := div_pos εpos Bpos
   rcases ct _ pos₀ with ⟨N₁, h₁⟩
-  sorry
+  use max N₀ N₁
+  intro n nge
+  calc
+    |s n * t n - 0| = |s n * (t n - 0)| := by congr ; ring
+    _ = |s n| * |t n - 0| := abs_mul _ _
+    _ ≤ B * |t n - 0| := mul_le_mul (le_of_lt (h₀ n (le_of_max_le_left nge))) (le_refl _) (abs_nonneg _) (le_of_lt Bpos)
+    _ < B * (ε / B) := mul_lt_mul' (le_refl _) (h₁ n (le_of_max_le_right nge)) (abs_nonneg _) Bpos
+    _ = ε := by field_simp
 
 theorem convergesTo_mul {s t : ℕ → ℝ} {a b : ℝ}
       (cs : ConvergesTo s a) (ct : ConvergesTo t b) :
@@ -80,7 +107,7 @@ theorem convergesTo_unique {s : ℕ → ℝ} {a b : ℝ}
       (sa : ConvergesTo s a) (sb : ConvergesTo s b) :
     a = b := by
   by_contra abne
-  have : |a - b| > 0 := by sorry
+  have : |a - b| > 0 := abs_pos.mpr (sub_ne_zero_of_ne abne)
   let ε := |a - b| / 2
   have εpos : ε > 0 := by
     change |a - b| / 2 > 0
@@ -88,9 +115,14 @@ theorem convergesTo_unique {s : ℕ → ℝ} {a b : ℝ}
   rcases sa ε εpos with ⟨Na, hNa⟩
   rcases sb ε εpos with ⟨Nb, hNb⟩
   let N := max Na Nb
-  have absa : |s N - a| < ε := by sorry
-  have absb : |s N - b| < ε := by sorry
-  have : |a - b| < |a - b| := by sorry
+  have absa : |s N - a| < ε := hNa N (le_max_left _ _)
+  have absb : |s N - b| < ε := hNb N (le_max_right _ _)
+  have : |a - b| < |a - b| := by calc
+    |a - b| = |(a - s N) + (s N - b)| := by congr ; ring
+    _ ≤ |a - s N| + |s N - b| := by apply abs_add
+    _ = |s N - a| + |s N - b| := by rw [abs_sub_comm]
+    _ < ε + ε := by apply add_lt_add absa absb
+    _ = |a - b| := by ring
   exact lt_irrefl _ this
 
 section
@@ -100,4 +132,3 @@ def ConvergesTo' (s : α → ℝ) (a : ℝ) :=
   ∀ ε > 0, ∃ N, ∀ n ≥ N, |s n - a| < ε
 
 end
-
